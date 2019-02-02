231cb15e13736c10b52f8fe2b04f3a9e11b34ba9
Mike Guastafeste <39768361+mguastafeste@users.noreply.github.com>
2019-02-02 12:12:24 -0500

    Update geolocationSOQL.sql

--- geolocationSOQL.sql
+++ geolocationSOQL.sql
@@ -1,7 +1,15 @@
-SELECT Name, Email, MailingAddress, MailingCity, MailingState, MailingCountry, MailingLatitude, MailingLongitude
-FROM Contact 
+SELECT Name, Phone, MailingAddress, MailingCity, MailingState, MailingCountry, MailingLatitude, MailingLongitude
+FROM Account 
 WHERE distance(MailingAddress, geolocation(37.775, -122.418), 'mi') < 50 
 ORDER BY distance(MailingAddress, geolocation(37.775, -122.418), 'mi')
 LIMIT 20;
 
---THE 2nd parameter for distance(1,2,3) is the point to measure FROM
+--The 1st parameter for distance(1,2,3) will be the clinics' location field.
+--The 2nd parameter for distance(1,2,3) is the point to measure FROM, ie zip code's location field.
+--Schedule DLRS to fill # of cases for current month for each source overnight.
+--When someone chooses a clinic, add +1 to the source's case count (to keep total accurate throughout day without dlrs).
+--...How do we get this number on the account/clinic record?
+--If rejected, log rejected reason & add to SOURCE.
+--Need both the zipcode and the clinic to contain geolocation info to measure distance.
+--Auto-add geolocation to existing accounts (does not support person accounts or custom address fields)
+--...using CLEAN RULES in setup (possibly only available in classic).
