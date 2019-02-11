0c3547c279f1b8392286ccbeefda7616ca286e4a
Mike Guastafeste <39768361+mguastafeste@users.noreply.github.com>
2019-02-11 17:51:37 -0500

    Update soqlTaskWhatIdRelationships.sql

--- soqlTaskWhatIdRelationships.sql
+++ soqlTaskWhatIdRelationships.sql
@@ -1,4 +1,4 @@
-SELECT Id, Subject, Owner.Name 
+SELECT Id, Subject, Owner.Name, What.Name 
 FROM Task 
 WHERE Status='Open' 
 AND WhatId IN (SELECT Id FROM litify_pm__Matter__c WHERE litify_pm__Status__c = 'Closed')
